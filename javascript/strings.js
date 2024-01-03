d33f0f35d6fc5205a857ed2a46f0e18ab9d486cf
Ahmed Raza <144887803+ahmedraza565@users.noreply.github.com>
2024-01-03 10:18:51 +0000

    strings and it's methods

--- javascript/strings.js
+++ javascript/strings.js
@@ -1,5 +1,21 @@
-let name = "Ahmed"
-let repoCount = 100;
+// let name = "Ahmed"
+// let repoCount = 100;
 
-console.log(`My name is ${name} and my repo count is ${repoCount}`);
+// console.log(`My name is ${name} and my repo count is ${repoCount}`);
 
+// const country = new String("Pakistan");
+// const province = "Sindh";
+
+// console.log(typeof(province));
+
+// const url = "https://hitesh.com/hitesh%20choudhary";
+
+// const newUrl = url.replace("%20", "-");
+
+// console.log(url);
+// console.log(newUrl);
+// console.log(url);
+
+const greeting = "    Hello World  !   ";
+console.log(greeting);
+console.log(greeting.trim());
