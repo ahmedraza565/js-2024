5e9dce3638d30a178bf6f2965fb77f81adccc0dc
Ahmed Raza <144887803+ahmedraza565@users.noreply.github.com>
2024-01-04 13:10:51 +0000

    dates in javascript

---
+++ javascript/dates.js
@@ -0,0 +1,28 @@
+// const myDate = new Date();
+
+// console.log(myDate.toDateString());
+
+// console.log(myDate.toString());
+
+// console.log(myDate.toLocaleString());
+
+// console.log(myDate.toISOString());
+
+
+// const newDate = new Date(2005, 4, 5);
+
+// const newDate = new Date(2005, 4, 5, 5, 3);
+
+// const newDate = new Date("12-5-2005")
+
+// console.log(newDate.toLocaleDateString());
+
+// const currentTime = Date.now()
+// const myDate = new Date("5-3-2022")
+
+// console.log(Math.floor(currentTime/1000));
+// console.log(myDate.getTime()/1000);
+
+// console.log(myDate.getMonth()+1);
+
+// In Javscript month starts from 0 -- note
