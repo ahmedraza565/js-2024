798eee668fbd50bc1ba2ba7eef465ca285124e71
Ahmed Raza <144887803+ahmedraza565@users.noreply.github.com>
2024-01-04 10:05:42 +0000

    numbers and maths

---
+++ javascript/numbers.js
@@ -0,0 +1,46 @@
+// const accountNumber = 1234234422;
+// console.log(accountNumber);
+
+// const accountNumber = new Number(2903423423);
+
+// const balance = new Number(100);
+// console.log(balance.toString());
+
+// console.log(balance.toFixed(2)); //adds 0s after the decimal point
+
+// const num = new Number(34.6);
+// console.log(num.toPrecision(2)); // rounds off the number to desired digits
+
+
+// const num = 1000000;
+// console.log(num.toLocaleString('en-IN'));
+
+// Maths 
+
+// const num = -23.4;
+
+// console.log(Math.abs(num)); // converts negative to positive
+
+// console.log(Math.round(num)); // rounds off number 
+
+// console.log(Math.ceil(num)); // rounds off to lesser value
+
+// console.log(Math.floor(num)); // rounds off to higher value
+
+// console.log(Math.min(3,4,32,73)); // finds min value 
+
+// // console.log(Math.max(3,4,32,73)); // finds max value
+
+// // Random 
+
+// // Math.random gives random values between 0 & 1
+
+// // console.log(Math.floor(Math.random()*10) + 1);
+
+// const min = 10;
+// const max = 20;
+
+// console.log(Math.floor(Math.random()* (max - min + 1) + min));
+
+
+
